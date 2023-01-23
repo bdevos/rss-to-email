cc3d126c7d19ee518f5abc5b63c5bec1f7332a1c
Berry de Vos <bdevos@gmail.com>
2023-01-23 16:43:51 +0100

    macrumors (#41)

--- src/feeds.ts
+++ src/feeds.ts
@@ -2,6 +2,9 @@ export const feeds = [
   // Daring Fireball
   'https://daringfireball.net/feeds/main',
 
+  // MacRumors
+  'https://feeds.macrumors.com/MacRumors-All',
+
   // Appjeniksaan
   'https://appjeniksaan.nl/feed.xml',
 ]
