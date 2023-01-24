c6a6de3e7fe4c7ba24fdbc0d94deb266bbc52159
Berry de Vos <bdevos@gmail.com>
2023-01-24 05:42:38 +0100

    switching back to daily

--- src/feeds.ts
+++ src/feeds.ts
@@ -2,9 +2,6 @@ export const feeds = [
   // Daring Fireball
   'https://daringfireball.net/feeds/main',
 
-  // MacRumors
-  'https://feeds.macrumors.com/MacRumors-All',
-
   // Appjeniksaan
   'https://appjeniksaan.nl/feed.xml',
 ]
