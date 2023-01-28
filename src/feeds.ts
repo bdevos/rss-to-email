0eb2c0e908b74847f6b1d7c9f98502331e7c2f03
Berry de Vos <bdevos@gmail.com>
2023-01-28 09:29:57 +0100

    add RSS feed for jakearchibald (#60)

--- src/feeds.ts
+++ src/feeds.ts
@@ -2,6 +2,9 @@ export const feeds = [
   // Daring Fireball
   'https://daringfireball.net/feeds/main',
 
+  // JakeArchibald.com
+  'https://jakearchibald.com/posts.rss',
+
   // Appjeniksaan
   'https://appjeniksaan.nl/feed.xml',
 ]
