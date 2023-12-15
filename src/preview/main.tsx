981b35c344a96c78c8b3ae4576089486ff4aae6e
Berry de Vos <bdevos@gmail.com>
2023-12-15 04:39:59 +0100

    Update some formatting issues since latest prettier

--- src/preview/main.tsx
+++ src/preview/main.tsx
@@ -6,5 +6,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <PreviewApp />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
